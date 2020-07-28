7fb733b489b3ad88c7dbeebf76a4af92845b25c2
Brian Tomlin <btomlin@homeaway.com>
2020-07-28 18:06:13 -0500

    Add deep linking and update link styles

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 (function(){
+
+    // Listeners
+    document.addEventListener("DOMContentLoaded", resolveUrl, false);
+
     document.addEventListener('click', function (event) {
         if (!event.target.classList.contains('menu-item')) return;
         var previousSelection = document.querySelector('.active-menu');
         if (previousSelection) previousSelection.classList.remove('active-menu');
         event.target.classList.add('active-menu');
-        var content = document.querySelector(event.target.hash);
-        var previous = document.querySelector('.visible');
-        if (previous) previous.classList.remove('visible');
-        if (content) content.classList.add('visible');
-
+        loadContentSection(event.target.hash);
         event.preventDefault();
     }, false);
+
+    // Functions
+
+    // Checks for a hash in the URL and updates the visible section if there is a match and also marks the correct menu item as selected
+    function resolveUrl() {
+        loadContentSection(location.hash);
+        var previousSelection = document.querySelector('.active-menu');
+        const newActiveLink = document.querySelector(`[href="${location.hash}"]`);
+        if(!newActiveLink) return;
+        if (previousSelection) previousSelection.classList.remove('active-menu');
+        newActiveLink.classList.add('active-menu');
+        
+    }
+
+    // Marks the correct content section visible based on the passed in hash
+    function loadContentSection(hash) {
+        var content = document.querySelector(hash);
+        var previous = document.querySelector('.visible');
+        if (previous && content) previous.classList.remove('visible');
+        if (content) {
+            content.classList.add('visible')
+            history.replaceState(null, null, hash);
+        };
+        
+    }
 })();
